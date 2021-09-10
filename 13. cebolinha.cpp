e0e16379e7dac9e69c30988881f79499f7d7bc00
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2021-09-10 17:55:23 -0300

    fix: changed when input has 2 Rs

--- 13. cebolinha.cpp
+++ 13. cebolinha.cpp
@@ -3,16 +3,21 @@
 
 int main(){
     char string[MAX];
+    char string_temp[MAX];
     scanf("%s", &string);
-
-    int i = 0;
+    int k = 0;
     for(int i = 0 ; string[i] != '\0' ; i++) {
 
-        if(string[i] == 'R'){
-            string[i] = 'L';
+        string_temp[k] = string[i];
+        if (string_temp[k] == 'R'){
+            string_temp[k] = 'L';
+            if (string[i+1] == 'R'){
+                i++;
+            }
         }
-    i++;
+    k++;
     }
-    printf("\n%s", string);
+    string_temp[k]='\0';
+    printf("\n%s", string_temp);
 
 }
