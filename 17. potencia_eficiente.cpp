1df1c02a02725803571b8b06acb1e8395956e747
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2021-09-11 02:03:47 -0300

    feat: added potencia eficiente

---
+++ 17. potencia_eficiente.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cmath>
+
+int potencia(int x, int n) {
+    if (n == 0) {
+        return 1;
+    }
+    if (n % 2 == 0) {
+        return pow(pow(x, (n / 2)), 2);
+    } else {
+        return x * pow(x, n-1);
+    }
+}
+
+int main() {
+    int x, n;
+    scanf("%d %d", &x, &n);
+    printf("%d", potencia(x, n));
+
+    return 0;
+}
