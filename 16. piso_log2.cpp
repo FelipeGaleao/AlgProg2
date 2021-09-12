fbde5857b620cd2efc9646669e32363d384fc709
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2021-09-12 11:03:16 -0300

    feat: added piso_log2 exercise

--- 16. piso_log2.cpp
+++ 16. piso_log2.cpp
@@ -5,16 +5,21 @@ int piso_log2(int N){
    int i, n;
    i = 0;
    n = 1;
-   while (n <= N/2) {
-      n = 2 * n;
-      i += 1;
+   if(n <= 2){
+      return 1;
+   }else{
+      i++;
+      return n <= N/2;
    }
    return i;    
 }
 int main(){
     int numero;
     scanf("%i", &numero);
-    numero = piso_log2(numero);
-    printf("O numero eh: %i", numero);
-    scanf("%i", numero);
+    while(numero < 1){
+       printf("Você precisa inserir um número maior que 1");
+    }
+    int resultado = piso_log2(numero);
+    printf("O logaritmo de %i na base 2 = %i", numero, resultado);
+    return 0;
 }
