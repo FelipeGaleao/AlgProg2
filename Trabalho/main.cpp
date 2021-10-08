c1f9595202953f4c08a4668c7fc9d9223ad932fa
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2021-10-08 17:37:45 -0300

    feat: adicionado algumas esboços de funções

---
+++ Trabalho/main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define MAX 100
+
+struct Estudante {
+  int RGA;
+  char nome[100];
+  double media;
+} Estudantes;
+
+/* Buscas */
+
+int buscaRGA(Estudante v[], int n, int RGA);
+int buscaNome(Estudante v[], int n, char nome[]);
+int buscaMedia(Estudante v[], int n, double media);
+
+void ordenaEstudantes(Estudante v[], int n, char op){};
+
+void imprimeEstudante(Estudante v[], int k);
+void imprimeEstudantes(Estudante v[], int n);
+
+void ler_estudantes(int qtdAlunos, Estudante Estudantes[MAX]) {
+  for (int i = 0; i < qtdAlunos; i++) {
+    scanf("%i", &Estudantes[i].RGA);
+    scanf(" %[^\n]s", &Estudantes[i].nome);
+    scanf("%lf", &Estudantes[i].media);
+  }
+}
+
+int main() {
+
+  int qtdAlunos;
+  Estudante Estudantes[MAX];
+
+  scanf("%d", &qtdAlunos);
+
+  ler_estudantes(qtdAlunos, Estudantes);
+
+  return 0;
+}
