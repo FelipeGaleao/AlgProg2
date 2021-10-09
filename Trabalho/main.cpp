39b4c6fdd6b1279b1f35436f012305367250ac35
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2021-10-09 02:18:34 -0300

    fix: added sort step

--- Trabalho/main.cpp
+++ Trabalho/main.cpp
@@ -13,10 +13,71 @@ int buscaRGA(Estudante v[], int n, int RGA);
 int buscaNome(Estudante v[], int n, char nome[]);
 int buscaMedia(Estudante v[], int n, double media);
 
-void ordenaEstudantes(Estudante v[], int n, char op){};
+void troca(Estudante &a, Estudante &b) {
+  Estudante tmp = a;
+  a = b;
+  b = tmp;
+}
+
+int separa(int p, int r, Estudante v[MAX], char opcao) {
+
+  char pivot;
+  Estudante pivotEstudante;
+
+  if (opcao == 'r') {
+    pivot = v[r].RGA;
+  }
+  if (opcao == 'n') {
+    pivotEstudante = v[r];
+  }
+  if (opcao == 'm') {
+    pivot = v[r].media;
+  }
+  int q = p;
+
+  for (int i = p; i < r; i++) {
+    if (opcao == 'r') {
+      if (v[i].RGA <= pivot) {
+        troca(v[i], v[q]);
+        q++;
+      }
+    }
+    if (opcao == 'n') {
+      if (v[i].nome <= pivotEstudante.nome) {
+        troca(v[i], v[q]);
+        q++;
+      }
+    }
+    if (opcao == 'm') {
+      if (v[i].media <= pivot) {
+        troca(v[i], v[q]);
+        q++;
+      }
+    }
+  }
+  troca(v[q], v[r]);
+  return q;
+}
+
+void quicksort(int p, int r, Estudante v[MAX], char opcao) {
+  int q;
+  if (p < r) {
+    q = separa(p, r, v, opcao);
+    q = separa(p, r, v, opcao);
+    quicksort(p, q - 1, v, opcao);
+    quicksort(q + 1, r, v, opcao);
+  }
+}
 
 void imprimeEstudante(Estudante v[], int k);
-void imprimeEstudantes(Estudante v[], int n);
+void imprimeEstudantes(Estudante v[], int n) {
+  for (int i = 0; i < n; i++) {
+    printf("RGA: %d \n", v[i].RGA);
+    printf("Nome: %s \n", v[i].nome);
+    printf("Media: %.2lf \n", v[i].media);
+    printf("------------ \n");
+  }
+}
 
 void ler_estudantes(int qtdAlunos, Estudante Estudantes[MAX]) {
   for (int i = 0; i < qtdAlunos; i++) {
@@ -39,7 +100,8 @@ int main() {
   scanf(" %c", &opcao);
   scanf("%d", &qtdBuscas);
 
-  s
+  quicksort(0, qtdAlunos - 1, Estudantes, opcao);
+  imprimeEstudantes(Estudantes, qtdAlunos);
 
   return 0;
 }
