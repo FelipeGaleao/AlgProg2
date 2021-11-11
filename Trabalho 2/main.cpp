0babb28ed150fd826d49a5332ff97e4c3bfc5d37
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2021-11-11 00:02:55 -0300

    feat: added method to search using name or cpf

--- Trabalho 2/main.cpp
+++ Trabalho 2/main.cpp
@@ -11,64 +11,92 @@ struct Pessoa {
 
 //   ======================> BUSCAS <======================
 
-Pessoa *buscaNome(Pessoa *lst, char *nome) {}
-Pessoa *buscaCPF(Pessoa *lst, int cpf) {}
+Pessoa *buscaNome(Pessoa *lst, char *nome) {
 
-void insere(Pessoa *lst, char *nome, int cpf){
-    Pessoa *nova;
-    nova = (Pessoa *) malloc(sizeof (Pessoa));
+  char nomeBuscado[101];
+  strcpy(nomeBuscado, nome);
 
-    strcpy(nova->nome, nome);
-    nova->cpf = cpf;
-    nova->nDoses = 0;
-    nova->prox = lst->prox;
-    lst->prox = nova;
+  if (lst->prox == NULL)
+    return NULL;
+  if (lst->prox->nome == nomeBuscado)
+    return lst->prox;
+
+  return buscaNome(lst->prox, nome);
+}
+
+Pessoa *buscaCPF(Pessoa *lst, int cpf) {
+
+  if (lst->prox == NULL)
+    return NULL;
+  if (lst->prox->cpf == cpf)
+    return lst->prox;
+  return buscaCPF(lst->prox, cpf);
+}
+
+void insere(Pessoa *lst, char *nome, int cpf) {
+  Pessoa *nova;
+  nova = (Pessoa *)malloc(sizeof(Pessoa));
+  strcpy(nova->nome, nome);
+  nova->cpf = cpf;
+  nova->nDoses = 0;
+  nova->prox = lst->prox;
+  lst->prox = nova;
 }
 
 //   ======================> REMOÇÃO <======================
 
-bool removePorNome(Pessoa *lst, char *nome){}
-bool removePorCPF(Pessoa *lst, int cpf){}
+bool removePorNome(Pessoa *lst, char *nome) {}
+bool removePorCPF(Pessoa *lst, int cpf) {}
 
 //   ======================> INCREMENTAR DOSES <======================
 
-bool IncrementaDosesPorNome(Pessoa *lst, char *nome){}
-bool incrementaDosesPorCPF(Pessoa *lst, int cpf){}
+bool IncrementaDosesPorNome(Pessoa *lst, char *nome) {}
+bool incrementaDosesPorCPF(Pessoa *lst, int cpf) {}
 
 //   ======================> IMPRESSÃO <======================
 
-void imprimePessoa(Pessoa *p){}
-void imprimeLista(Pessoa *lst){
+void imprimePessoa(Pessoa *p) {
+  Pessoa *pessoa;
+  pessoa = p;
+  printf("------------\nPessoa de nome: %s \nCPF: %d\nNum . de doses aplicadas "
+         ": %d \n------------\n",
+         pessoa->nome, pessoa->cpf, pessoa->nDoses);
+}
 
-    Pessoa *p;
+void imprimeLista(Pessoa *lst) {
 
-    for(p = lst; p != NULL; p = p->prox)
-        printf("------------\nPessoa de nome: %s \nCPF: %d\nNum . de doses aplicadas : %d \n------------\n", p->nome, p->cpf, p->nDoses);
+  Pessoa *p;
+
+  for (p = lst; p != NULL; p = p->prox)
+    printf("------------\nPessoa de nome: %s \nCPF: %d\nNum . de doses "
+           "aplicadas : %d \n------------\n",
+           p->nome, p->cpf, p->nDoses);
 }
 
 //   ======================> Limpar Listas <======================
 
-void limpaLista(Pessoa **lst){}
-
+void limpaLista(Pessoa **lst) {}
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    char nome_pessoa[101];
-    int cpf;
+  int n;
+  scanf("%d", &n);
+  char nome_pessoa[101];
+  int cpf;
 
-    Pessoa *lst;
-    
-    lst = (Pessoa*) malloc(sizeof(Pessoa));
-    lst->prox = NULL;
+  Pessoa *lst;
 
-    for(int i = 0; i < n; i++){
-       scanf(" %[^\n]s", nome_pessoa);
-       scanf("%d", &cpf);
-       insere(lst, nome_pessoa, cpf);
-    }
+  lst = (Pessoa *)malloc(sizeof(Pessoa));
+  lst->prox = NULL;
 
-    imprimeLista(lst->prox);
+  for (int i = 0; i < n; i++) {
+    scanf(" %[^\n]s", nome_pessoa);
+    scanf("%d", &cpf);
+    insere(lst, nome_pessoa, cpf);
+  }
 
-    return 0;
+  // imprimeLista(lst->prox);
+  cpf = 78910;
+  imprimePessoa(buscaCPF(lst, cpf));
+
+  return 0;
 }
