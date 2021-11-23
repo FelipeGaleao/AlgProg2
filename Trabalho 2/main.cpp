1f51fffeb257edaba8ad3099f7fb415bd0d65428
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2021-11-22 21:09:41 -0300

    fix: adjusts to pass on ava

--- Trabalho 2/main.cpp
+++ Trabalho 2/main.cpp
@@ -1,259 +1,265 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
 
-struct Pessoa {
-  char nome[101];
-  int cpf;
-  int nDoses;
-  Pessoa *prox;
-};
+  struct Pessoa {
+    char nome[101];
+    int cpf;
+    int nDoses;
+    Pessoa *prox;
+  };
 
-//   ======================> BUSCAS <======================
+  //   ======================> BUSCAS <======================
 
-Pessoa *buscaNome(Pessoa *lst, char *nome) {
+  Pessoa *buscaNome(Pessoa *lst, char *nome) {
 
-  if (lst == NULL)
-    return NULL;
+    if (lst == NULL)
+      return NULL;
 
-  if (strcmp(lst->nome, nome) == 0)
-    return lst;
+    if (strcmp(lst->nome, nome) == 0)
+      return lst;
 
-  return buscaNome(lst->prox, nome);
-}
-
-Pessoa *buscaCPF(Pessoa *lst, int cpf) {
-
-  if (lst == NULL)
-    return NULL;
-
-  if (lst->cpf == cpf)
-    return lst;
-
-  return buscaCPF(lst->prox, cpf);
-}
-
-void insere(Pessoa *lst, char *nome, int cpf) {
-
-  Pessoa *nova;
-
-  nova = (Pessoa *)malloc(sizeof(Pessoa));
-  strcpy(nova->nome, nome);
-  nova->cpf = cpf;
-  nova->nDoses = 0;
-  nova->prox = lst->prox;
-
-  lst->prox = nova;
-}
-
-//   ======================> REMOÇÃO <======================
-
-bool removePorNome(Pessoa *lst, char *nome) {
-
-  Pessoa *nova, *aux;
-  nova = lst;
-  aux = lst->prox;
-
-  while (aux != NULL && strcmp(aux->nome, nome) != 0) {
-    nova = aux;
-    aux = aux->prox;
+    return buscaNome(lst->prox, nome);
   }
-  if (aux != NULL) {
-    nova->prox = aux->prox;
-    free(aux);
-    return true;
+
+  Pessoa *buscaCPF(Pessoa *lst, int cpf) {
+
+    if (lst == NULL)
+      return NULL;
+
+    if (lst->cpf == cpf)
+      return lst;
+
+    return buscaCPF(lst->prox, cpf);
   }
-  
 
-  return false;
-}
+  void insere(Pessoa *lst, char *nome, int cpf) {
 
-bool removePorCPF(Pessoa *lst, int cpf) {
+    Pessoa *nova;
 
-  Pessoa *nova, *aux;
-  nova = lst;
-  aux = lst->prox;
+    nova = (Pessoa *)malloc(sizeof(Pessoa));
+    strcpy(nova->nome, nome);
+    nova->cpf = cpf;
+    nova->nDoses = 0;
+    nova->prox = lst->prox;
 
-  while (aux != NULL && aux->cpf != cpf) {
+    lst->prox = nova;
+  }
+
+  //   ======================> REMOÇÃO <======================
+
+  bool removePorNome(Pessoa *lst, char *nome) {
+
+    Pessoa *nova, *aux;
     nova = lst;
-    aux = aux->prox;
-  }
-  if (aux != NULL) {
-    nova->prox = aux->prox;
+    aux = lst->prox;
+
+    while (aux != NULL && strcmp(aux->nome, nome) != 0) {
+      nova = aux;
+      aux = aux->prox;
+    }
+    if (aux != NULL) {
+      nova->prox = aux->prox;
+      free(aux);
+      return true;
+    }
+
     free(aux);
-    return true;
-  }
-  return false;
-}
-
-//   ======================> INCREMENTAR DOSES <======================
-
-bool IncrementaDosesPorNome(Pessoa *lst, char *nome) {
-  Pessoa *pessoa = lst;
-  while (pessoa != NULL && strcmp(pessoa->nome, nome) != 0) {
-    pessoa = pessoa->prox;
-  }
-  if (pessoa != NULL && pessoa->nDoses < 2) {
-    pessoa->nDoses++;
-    return true;
+    return false;
   }
 
-  free(pessoa);
+  bool removePorCPF(Pessoa *lst, int cpf) {
 
-  return false;
-}
-bool incrementaDosesPorCPF(Pessoa *lst, int cpf) {
-  Pessoa *pessoa = lst;
-  while (pessoa != NULL && pessoa->cpf != cpf) {
-    pessoa = pessoa->prox;
+    Pessoa *nova, *aux;
+    nova = lst;
+    aux = lst->prox;
+
+    while (aux != NULL && aux->cpf != cpf) {
+      nova = aux;
+      aux = aux->prox;
+    }
+    if (aux != NULL) {
+      nova->prox = aux->prox;
+      free(aux);
+      return true;
+    }
+    free(aux);
+    return false;
   }
-  if (pessoa != NULL && pessoa->nDoses < 2) {
-    pessoa->nDoses++;
-    return true;
-  } 
-  return false;
-}
 
-//   ======================> IMPRESSÃO <======================
+  //   ======================> INCREMENTAR DOSES <======================
 
-void imprimePessoa(Pessoa *p) {
-  Pessoa *pessoa;
-  pessoa = p;
+  bool IncrementaDosesPorNome(Pessoa *lst, char *nome) {
+    Pessoa *pessoa = lst;
+    while (pessoa != NULL && strcmp(pessoa->nome, nome) != 0) {
+      pessoa = pessoa->prox;
+    }
+    if (pessoa != NULL && pessoa->nDoses < 2) {
+      pessoa->nDoses++;
+      return true;
+    }
 
-  if(p->nome != NULL){
-  printf("------------\nPessoa de nome: %s \nCPF: %d\nNum. de doses aplicadas"
-         ": %d \n------------\n",
-         pessoa->nome, pessoa->cpf, pessoa->nDoses);
-  }else{
-      printf("------------\nPessoa de nome: null \nCPF: null\nNum. de doses aplicadas"
-         ": null \n------------\n");
+    free(pessoa);
+
+    return false;
   }
-}
-
-void imprimeLista(Pessoa *lst) {
-
-  Pessoa *p;
-
-  for (p = lst->prox; p->nome != NULL; p = p->prox)
-    printf("------------\nPessoa de nome: %s \nCPF: %d\nNum. de doses "
-           "aplicadas: %d \n------------\n",
-           p->nome, p->cpf, p->nDoses);
-
-}
-
-//   ======================> Limpar Listas <======================
-
-void limpaLista(Pessoa **lst) {
-  Pessoa *p = *lst;
-  while (p != NULL) {
-    Pessoa *aux = p->prox;
-    free(p);
-    p = aux;
+  bool incrementaDosesPorCPF(Pessoa *lst, int cpf) {
+    Pessoa *pessoa = lst;
+    while (pessoa != NULL && pessoa->cpf != cpf) {
+      pessoa = pessoa->prox;
+    }
+    if (pessoa != NULL && pessoa->nDoses < 2) {
+      pessoa->nDoses++;
+      return true;
+    } 
+    return false;
   }
-}
 
-int main() {
-  int n;
-  char nome_pessoa[101];
-  char op;
-  char op2;
-  int cpf;
+  //   ======================> IMPRESSÃO <======================
 
-  Pessoa *lst;
-
-  /* Descobre a qtde de pessoas que serão inseridas na lista */
-  scanf("%d", &n);
-
-  lst = (Pessoa *)malloc(sizeof(Pessoa));
-  lst->prox = NULL;
-
-  for (int i = 0; i < n; i++) {
-    scanf(" %[^\n]s", nome_pessoa);
-    scanf("%d", &cpf);
-    insere(lst, nome_pessoa, cpf);
-  }
-  while (op != 'x') {
-    bool resultado;
-
-    scanf(" %c", &op);
-    scanf(" %c", &op2);
-
-    switch (op) {
-
-    // Opção de Impressão de lista
-    case 'i':
-      switch (op2) {
-
-      // Opção Imprimir por Nome
-      case 'n':
-        scanf(" %[^\n]s", nome_pessoa);
-        imprimePessoa(buscaNome(lst, nome_pessoa));
-        break;
-        
-        // Opção Imprimir por CPF
-      case 'c':
-        scanf("%i", &cpf);
-        imprimePessoa(buscaCPF(lst, cpf));
-        break;
-      }
-      break;
-
-    // Opção para Remover da Lista
-    case 'r':
-      switch (op2) {
-
-        // Opção Remover por Nome
-      case 'n':
-        scanf(" %[^\n]s", nome_pessoa);
-        resultado = removePorNome(lst, nome_pessoa);
-        if (resultado) {
-          printf("OK! \n");
-        } else {
-          printf("FAIL! \n");
-        }
-        break;
-        // Opção Remover por CPF
-      case 'c':
-        scanf("%i", &cpf);
-        resultado = removePorCPF(lst, cpf);
-        if (resultado) {
-          printf("OK! \n");
-        } else {
-          printf("FAIL! \n");
-        }
-        break;
-      }
-      break;
-
-    // Incrementar dose
-    case 'd':
-      switch (op2) {
-
-        // Incrementar dose por Nome
-      case 'n':
-        scanf(" %[^\n]s", nome_pessoa);
-        resultado = IncrementaDosesPorNome(lst, nome_pessoa);
-        if (resultado) {
-          printf("OK! \n");
-        } else {
-          printf("FAIL! \n");
-        }
-        break;
-      // Incrementar dose por CPF
-      case 'c':
-        scanf("%i", &cpf);
-        resultado = incrementaDosesPorCPF(lst, cpf);
-        if (resultado) {
-          printf("OK! \n");
-        } else {
-          printf("FAIL! \n");
-        }
-        break;
-      }
-      break;
+  void imprimePessoa(Pessoa *p) {
+    if (p->nome != NULL) {
+      printf("------------\n");
+      printf("Pessoa de nome: %s\n", p->nome);
+      printf("CPF: %d\n", p->cpf);
+      printf("Num. de doses aplicadas: %d\n", p->nDoses);
+      printf("------------\n");
+    } else {
+      printf("------------\n");
+      printf("Pessoa de nome: null\n");
+      printf("CPF: null\n");
+      printf("Num. de doses aplicadas: null\n");
+      printf("------------\n");
     }
   }
-    imprimeLista(lst);
+
+  void imprimeLista(Pessoa *lst) {
+
+    Pessoa *p;
+
+    for (p = lst->prox; p->nome != NULL; p = p->prox){
+      printf("------------\n");
+      printf("Pessoa de nome: %s\n", p->nome);
+      printf("CPF: %d\n", p->cpf);
+      printf("Num. de doses aplicadas: %d\n", p->nDoses);
+      printf("------------\n");
+    }
+  }
+
+  //   ======================> Limpar Listas <======================
+
+  void limpaLista(Pessoa **lst) {
+    Pessoa *p = *lst;
+    while (p != NULL) {
+      Pessoa *aux = p->prox;
+      free(p);
+      p = aux;
+    }
+  }
+
+  int main() {
+    int n;
+    char nome_pessoa[101];
+    char op;
+    char op2;
+    int cpf;
+
+    Pessoa *lst;
+
+    /* Descobre a qtde de pessoas que serão inseridas na lista */
+    scanf("%d", &n);
+
+    lst = (Pessoa *)malloc(sizeof(Pessoa));
+    lst->prox = NULL;
+
+    for (int i = 0; i < n; i++) {
+      scanf(" %[^\n]s", nome_pessoa);
+      scanf("%d", &cpf);
+      insere(lst, nome_pessoa, cpf);
+    }
+    while (op != 'x') {
+      bool resultado;
+
+      scanf(" %c", &op);
+      scanf(" %c", &op2);
+      switch (op) {
+
+      // Opção de Impressão de lista
+      case 'i':
+        switch (op2) {
+
+        // Opção Imprimir por Nome
+        case 'n':
+          scanf(" %[^\n]s", nome_pessoa);
+          imprimePessoa(buscaNome(lst, nome_pessoa));
+          break;
+
+          // Opção Imprimir por CPF
+        case 'c':
+          scanf("%i", &cpf);
+          imprimePessoa(buscaCPF(lst, cpf));
+          break;
+        }
+        break;
+
+      // Opção para Remover da Lista
+      case 'r':
+        switch (op2) {
+
+          // Opção Remover por Nome
+        case 'n':
+          scanf(" %[^\n]s", nome_pessoa);
+          resultado = removePorNome(lst, nome_pessoa);
+          if (resultado) {
+            printf("OK! \n");
+          } else {
+            printf("FAIL! \n");
+          }
+          break;
+          // Opção Remover por CPF
+        case 'c':
+          scanf("%i", &cpf);
+          resultado = removePorCPF(lst, cpf);
+          if (resultado) {
+            printf("OK! \n");
+          } else {
+            printf("FAIL! \n");
+          }
+          break;
+        }
+        break;
+
+      // Incrementar dose
+      case 'd':
+        switch (op2) {
+
+          // Incrementar dose por Nome
+        case 'n':
+          scanf(" %[^\n]s", nome_pessoa);
+          resultado = IncrementaDosesPorNome(lst, nome_pessoa);
+          if (resultado) {
+            printf("OK! \n");
+          } else {
+            printf("FAIL! \n");
+          }
+          break;
+        // Incrementar dose por CPF
+        case 'c':
+          scanf("%i", &cpf);
+          resultado = incrementaDosesPorCPF(lst, cpf);
+          if (resultado) {
+            printf("OK! \n");
+          } else {
+            printf("FAIL! \n");
+          }
+          break;
+        }
+        break;
+      }
+    }
+    if(op == 'x'){
+      imprimeLista(lst);
+    }
     limpaLista(&lst);
-  return 0;
-}
+    return 0;
+  }
